dc2be4ceef645bc1169775617e3dd6478f7d26af
Vígh Dániel <vighd@digital.co.hu>
2022-07-25 10:45:01 +0200

    Update configs

--- configs/usr/local/bin/trackpoint_configuration.sh
+++ configs/usr/local/bin/trackpoint_configuration.sh
@@ -5,10 +5,10 @@ while ! $CONFIG_SET; do
   if [ ! -f "/sys/devices/platform/i8042/serio1/serio2/sensitivity" ]; then
     sleep 1
   else
-    echo -n 2 > /sys/devices/platform/i8042/serio1/serio2/drift_time
-    echo -n 132 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
-    echo -n 158 > /sys/devices/platform/i8042/serio1/serio2/speed
-    echo -n 6 > /sys/devices/platform/i8042/serio1/serio2/inertia
+    echo -n 1 > /sys/devices/platform/i8042/serio1/serio2/drift_time
+    #echo -n 132 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+    #echo -n 158 > /sys/devices/platform/i8042/serio1/serio2/speed
+    #echo -n 6 > /sys/devices/platform/i8042/serio1/serio2/inertia
     CONFIG_SET=true
   fi
 done
